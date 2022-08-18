d4955bc26f27969e9dcbadb6e057da6b65500d7b
worachote1 <timeworachote2002@gmail.com>
2022-08-19 03:05:29 +0700

    end stack problem

--- CE_Kmitl_Lab/ch3_Stack/stack_ex5.py
+++ CE_Kmitl_Lab/ch3_Stack/stack_ex5.py
@@ -1,76 +1,80 @@
-from inspect import stack
-from pickle import STACK_GLOBAL
-
-
 class Stack:
-    # __ 
+    # __
     def __init__(self):
-        self._data = [];
-        self._size = 0;
+        self._data = []
+        self._size = 0
 
-    def push(self,data):
-        self._data.append(data);
-        self._size+=1;
+    def push(self, data):
+        self._data.append(data)
+        self._size += 1
 
     def pop(self):
-        if(self._size==0):
-            return"Can not pop";
-        self._size-=1;
-        return self._data.pop();
+        if(self._size == 0):
+            return"Can not pop"
+        self._size -= 1
+        return self._data.pop()
 
     def peek(self):
-        return self._data[-1];
+        return self._data[-1]
 
     def getData(self):
-        return self._data;
+        return self._data
 
     def size(self):
-        return self._size;
+        return self._size
 
     def isEmpty(self):
-        if(self._size==0):
-            return True;
-        return False;
+        if(self._size == 0):
+            return True
+        return False
 
     def items(self):
-        return self._data;
+        return self._data
 
-inp = input("Enter Input : ").split(",");
-tree = Stack();
-hight = [];
-#print(inp);
+
+inp = input("Enter Input : ").split(",")
+tree = Stack()
+hight = []
+# print(inp);
 for item in inp:
-    if(item[0]=="A"):
+    if(item[0] == "A"):
         if(tree.isEmpty()):
-            tree.push(item);
-            hight.append(int(item[1:]));
-            continue;
+            tree.push(item)
+            hight.append(int(item[1:]))
+            continue
 
-        tree.push(item);
-        hight.append(int(item[1:]));
+        tree.push(item)
+        hight.append(int(item[1:]))
 
-    if(item=="B"):
-        #clone temp
-        temp_hight = [];
+    if(item == "B"):
+        # clone temp
+        temp_hight = []
         for h in hight:
-            temp_hight.append(h);
-        
-        print("now temp = ",temp_hight);
+            temp_hight.append(h)
+        #print("now temp = ", temp_hight)
+        count = 0
+        if(len(temp_hight) == 1):
+            count += 1
+        else:
+            for i in range(len(temp_hight)-1, 0-1, -1):
+                temp_hight_2 = []
+                if(i == len(temp_hight)-1):
+                    count += 1
+                    # print("first prn "+str(i)+" which is : "+str(temp_hight[i]));
+                    continue
+                passCheck = temp_hight[i+1:]
+                # print(passCheck);
+                check = True
+                for j in passCheck:
+                    if(temp_hight[i] <= j):
+                        check = False
+                        break
+                # count+=1;
+                if(check):
+                    count += 1
+                    # print("prn "+str(i)+" which is : "+str(temp_hight[i]));
 
-        if(len(temp_hight)==0):
-            print(0);
-            continue;
-        if(len(temp_hight)==1):
-            print(1);
-            continue;
-        count = 0;
-        #loop from last to 1 index
-        # for i in range(len(temp_hight)-1, 1-1, -1):
-        #     if(temp_hight[i]<temp_hight[i-1]):
-        #         count+=1;
-            
-        # print(count);
-        
+        print(count)
 
     if(item=="S"):
         for i in range(len(hight)):
@@ -78,6 +82,6 @@ for item in inp:
                 hight[i]-=1;
                 continue;
             hight[i]+=2;
-    
+
 
 # prn bug : A 4,A 3,B
--- CE_Kmitl_Lab/ch3_Stack/test_prn.py
+++ CE_Kmitl_Lab/ch3_Stack/test_prn.py
@@ -1,6 +1,6 @@
 
 count = 0;
-temp_hight = [9, 11, 7, 9, 7, 1];
+temp_hight = [17, 19, 15, 17, 15, 9, 55, 39];
 #loop from last to index 1
 if(len(temp_hight)==1):
     count+=1;
@@ -9,10 +9,10 @@ else:
         temp_hight_2 = [];
         if(i==len(temp_hight)-1):
             count+=1;
-            print("first prn "+str(i)+" which is : "+str(temp_hight[i]));
+            #print("first prn "+str(i)+" which is : "+str(temp_hight[i]));
             continue;
         passCheck = temp_hight[i+1:];
-        print(passCheck);
+        #print(passCheck);
         check = True;
         for j in passCheck:
             if(temp_hight[i]<=j):
@@ -21,10 +21,6 @@ else:
         #count+=1;
         if(check):
             count+=1;
-            print("prn "+str(i)+" which is : "+str(temp_hight[i]));
+            #print("prn "+str(i)+" which is : "+str(temp_hight[i]));
         
-    #check at index 0
-    # if(temp_hight[0]>temp_hight[1]):
-    #     count+=1;
-    #     print("last prn "+str(i)+" which is : "+str(temp_hight[0]));
 print(count);
