40839f279adc14222dcf0ae0a10e3b7f4283cd6d
worachote1 <timeworachote2002@gmail.com>
2022-08-18 07:46:09 +0700

    prn check InToPsot

--- CE_Kmitl_Lab/ch3_Stack/stack_ex3.py
+++ CE_Kmitl_Lab/ch3_Stack/stack_ex3.py
@@ -1,95 +1,79 @@
-class StackCalc:
-
+class Stack:
+    # __ 
     def __init__(self):
         self._data = [];
-        self._resut = 0;
         self._size = 0;
 
-    def push(self, data):
-        self._data.append(data)
-        self._size += 1
+
+    def push(self,data):
+        self._data.append(data);
+        self._size+=1;
 
     def pop(self):
-        if(self._size == 0):
-            return"Can not pop"
-        self._size -= 1
-        return self._data.pop()
+        if(self._size==0):
+            return"Can not pop";
+        self._size-=1;
+        return self._data.pop();
 
     def peek(self):
-        return self._data[-1]
+        return self._data[-1];
 
     def getData(self):
-        return self._data
+        return self._data;
 
     def size(self):
-        return self._size
+        return self._size;
 
     def isEmpty(self):
-        if(self._size == 0):
-            return True
-        return False
+        if(self._size==0):
+            return True;
+        return False;
 
-    def getValue(self):
-        return int(self._resut);
+    def items(self):
+        return self._data;
 
-    def run(self, arg):
-        # store,cal section
-        opt = ["+", "-", "*", "/", "DUP", "POP", "PSH"];
-        char_num = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-        for item in arg:
-            if((item not in opt) and (item[0] not in char_num)):
-                print("Invalid instruction: "+str(item));
-                exit();
-        #    self.push(item);
-        # for item in self.getData():
-        #     print("prn 44 -> "+item);
-            num1 = 0;
-            num2 = 0;
-            # normal operator
-            if(item in opt[0:4]):
-                if(item == "+"):
-                    num1 = int(self.pop());
-                    num2 = int(self.pop());
-                    self.push(num1+num2);
-                elif(item == "-"):
-                    num1 = int(self.pop());
-                    num2 = int(self.pop());
-                    self.push(num1-num2);
-                elif(item == "*"):
-                    num1 = int(self.pop());
-                    num2 = int(self.pop());
-                    self.push(num1*num2);
-                elif(item=="/"):
-                    num1 = int(self.pop());
-                    num2 = int(self.pop());
-                    self.push(num1/num2);
-                continue;
-            #extra operator
-            if(item in opt[4:len(opt)]):
-                if(item == "DUP"):
-                    self.push(self._data[-1]);
-                    # print("prn found DUP");
-                elif(item == "POP"):
-                    self.pop();
-                    # print("prn found POP");
-                elif(item == "PSH"):
-                    pass;
-                # print("Acds");
-                continue;  
-                
-            #if not any operator then push to stack
-            self.push(item);
-            # print("rgh item -> "+item);
-        
-        #end cal
-        if(self.isEmpty()):
-            self._resut = 0;
-            return;
-        self._resut = self.peek();
+#prn main
+# K+L-M*N+(O^P)*W/U/V*T+Q
+inp = input('Enter Infix : ')
+
+S = Stack()
+result = "";
+
+#cal section
+#operator presence
+# 1) (),{},[]
+# 2) ^   : R->L
+# 3) *,/ : L->R
+# 4) +,- : L->R
+opt = {"+":1,"-":1,"*":2,"/":2,"^":3};
+#print("prn -> ",opt());
+for item in inp:
     
-print("* Stack Calculator *");
-arg = input("Enter arguments : ").split(" ");
+    print("prn -> ",opt.get('*'));
+    
+    if(item not in opt):
+        S.push(item);
+        continue;
+    if(item=="^"):
+        S.push(item);
+        continue;
+    if(opt.get(item)>opt.get(S.peek())):
+        S.push(item);
+        continue;
 
-machine = StackCalc();
-machine.run(arg)
-print(machine.getValue())
+    while(opt.get(item)<=opt.get(S.peek()) ):
+        if(S.isEmpty() or S.peek()=='('):
+            S.push(item);
+            break;
+        result += S.pop();
+
+
+
+#end loop for in
+
+# print('Postfix : ', end='');
+# print(result,end="");
+# while not S.isEmpty():
+#     print(S.pop(), end='');
+
+# print()
--- CE_Kmitl_Lab/ch3_Stack/test_prn.py
+++ CE_Kmitl_Lab/ch3_Stack/test_prn.py
@@ -2,3 +2,15 @@ s = ['K', 'M', 'I', 'T', 'L', 'C', 'o', '2', '5', '6', '3'];
 prn = ''.join([str(elem) for elem in s]); #array to string
 print(prn);
 print(prn[len(prn)-2:])
+print(s);
+
+opt = ["+","-","*","/","^"];
+print(opt[0:2]);
+print(opt[2:4]);
+print(opt[-1]);
+
+my_dict = {"+":1,"-":1,"*":2,"/":2,"^":3};
+print(my_dict)
+for item in my_dict:
+    print(item);
+print(my_dict.get('-'));
