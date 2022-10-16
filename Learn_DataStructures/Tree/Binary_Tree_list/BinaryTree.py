566a57ec96ff8a60f2ef64f09e940225c05666c8
worachote1 <timeworachote2002@gmail.com>
2022-10-16 12:53:53 +0700

    level order using Queue

--- Learn_DataStructures/Tree/Binary_Tree_list/BinaryTree.py
+++ Learn_DataStructures/Tree/Binary_Tree_list/BinaryTree.py
@@ -1,3 +1,18 @@
+class Queue:
+    def __init__(self):
+        self.q = []
+    def Enqueue(self,i):
+        self.q.append(i)
+    def Dequeue(self):
+        if(len(self.q)==0):
+            return ""
+        return self.q.pop(0)
+    def isEmpty(self):
+        return len(self.q) == 0
+    def size(self):
+        return len(self.q)
+    def show(self):
+        return self.q.copy()
 
 class TreeNode:
     def __init__(self,data):
@@ -29,6 +44,18 @@ def postOrderTraversal(rootNode):
     postOrderTraversal(rootNode.rightChild)
     print(rootNode.data)
 
+# display each sub tree in the same hight
+def levelOrderTraversal(rootNode):
+    prn = Queue()
+    prn.Enqueue(rootNode)
+    while(not prn.isEmpty()):
+        q = prn.Dequeue()
+        if(q.leftChild != None):
+            prn.Enqueue(q.leftChild)
+        if(q.rightChild != None):
+            prn.Enqueue(q.rightChild)
+        print(q.data)
+
 newBT = TreeNode("Drinks") 
 leftChild = TreeNode("Hot")
 rightChild = TreeNode("Cold") 
@@ -49,3 +76,4 @@ inOrderTraversal(newBT)
 print("--- post order ---")
 postOrderTraversal(newBT)
 print("--- level order ---")
+levelOrderTraversal(newBT)
