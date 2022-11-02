0c65dede6b699d3e303c117c72ec8fd7dded0729
worachote1 <timeworachote2002@gmail.com>
2022-11-02 23:36:09 +0700

    Three some

--- Leet_Code/b44Nov_list/ex_15.py
+++ Leet_Code/b44Nov_list/ex_15.py
@@ -1,11 +1,25 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]] :
-        pass
+        return self.helper(nums)
+    
     def helper(self,nums: list[int]):
         res = []
         nums.sort()
         for i in range(len(nums)):
-            pL = 0
-            pR = len(nums)-1
-
-
+            pL = i+1
+            pR = len(nums)-1            
+            while(True):
+                if(pL>=pR):
+                    break
+                sum = nums[pL]+nums[i]+nums[pR]
+                if(sum<0):
+                    pL+=1
+                elif(sum>0):
+                    pR-=1
+                else:
+                    data = [nums[i],nums[pL],nums[pR]]
+                    if(data not in res):
+                        res.append(data)
+                    pL += 1
+                    pR -= 1
+        return res
