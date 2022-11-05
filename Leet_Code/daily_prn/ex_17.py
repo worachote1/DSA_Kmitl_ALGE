a3fe67fe4074364d1d40c68945d4e909fe092dd9
worachote1 <timeworachote2002@gmail.com>
2022-11-05 23:38:55 +0700

    Phone possible letter

--- Leet_Code/daily_prn/ex_17.py
+++ Leet_Code/daily_prn/ex_17.py
@@ -3,14 +3,20 @@ class Solution:
         data = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
                 "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
 
-        if(len(digits)==0):
+        if(len(digits) == 0):
             return []
-        if(len(digits)==1):
-            return [item for item in digits]
+        if(len(digits) == 1):
+            return [item for item in data[digits]]
 
         res = []
-        self.helper(digits,data,res)
+        self.helper(digits, data, res, "", 0)
         return res
 
-    def helper(self,digit_ss,data_map,res):
-        pass
+    def helper(self, digit_ss: str, data_map: dict, res: list, ss: str, idx: int):
+
+        if(idx > len(digit_ss)-1):
+            res.append(ss)
+            return
+        for item in data_map[digit_ss[idx]]:
+            new_string = ss+item
+            self.helper(digit_ss, data_map, res, new_string, idx+1)
--- Leet_Code/daily_prn/test_daily_prn.py
+++ Leet_Code/daily_prn/test_daily_prn.py
@@ -1,3 +1,4 @@
-x = "abc"
-v = [item for item in x]
-print(v)
+x = {"a":"13","b":"44","c":"35"}
+
+for item in x.keys():
+    print(item)
