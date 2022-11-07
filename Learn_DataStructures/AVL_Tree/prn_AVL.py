b014033c76a85357ee73442d45e827469b1ed6c9
worachote1 <timeworachote2002@gmail.com>
2022-11-07 11:30:40 +0700

    AVL Insert template

--- Learn_DataStructures/AVL_Tree/prn_AVL.py
+++ Learn_DataStructures/AVL_Tree/prn_AVL.py
@@ -105,6 +105,39 @@ def leftRotate(disbalanceNode:AVLNode):
     newRoot.height = 1+max(getHeight(newRoot.leftChild),getHeight(newRoot.rightChild))
 
     return newRoot
+
+def getBalance(rootNode:AVLNode):
+    if(rootNode == None):
+        return 0
+    return getHeight(rootNode.leftChild)-getHeight(rootNode.rightChild)
+
+def insertNode(rootNode:AVLNode,val:int):
+    if(rootNode == None):
+        return AVLNode(val)
+    elif(val < rootNode.data):
+        rootNode.leftChild = insertNode(rootNode.leftChild,val)
+    else:
+        rootNode.rightChild = insertNode(rootNode.rightChild,val)
+
+    rootNode.height = 1+max(getHeight(rootNode.leftChild),getHeight(rootNode.rightChild))
+    balance = getBalance(rootNode) #Identify which condition is in Case II 
+    #Left Left condition
+    if (balance > 1 and val < rootNode.leftChild.data):
+        return rightRotate(rootNode)
+    #Left Right condition
+    if(balance > 1 and  val > rootNode.leftChild.data):
+        rootNode.leftChild = leftRotate(rootNode.leftChild)
+        return rightRotate(rootNode)
+    #Right Right condition
+    if(balance < -1 and val > rootNode.rightChild.data):
+        return leftRotate(rootNode)
+    #Right Left condition
+    if(balance < -1 and val < rootNode.rightChild.data):
+        rootNode.rightChild = rightRotate(rootNode.rightChild)
+        return leftRotate(rootNode)
+    
+    return rootNode
+
 # End Insert Section .....
 
 # print tree method from KMITL
