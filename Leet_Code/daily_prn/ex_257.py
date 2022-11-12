ab22ccb1895962e235bcf9cdd7ef4f8d6ee7820e
worachote1 <timeworachote2002@gmail.com>
2022-11-13 01:40:13 +0700

    BST path

--- Leet_Code/daily_prn/ex_257.py
+++ Leet_Code/daily_prn/ex_257.py
@@ -4,4 +4,18 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+    def binaryTreePaths(self, root: TreeNode) -> list[str]:
+        res = []
+        self.helper(root,res,"")
+        return res
+    
+    def helper(self,root : TreeNode,res : list,ss_data : str):
+        if(root == None):
+            return
+        if(root.left == None and root.right == None):
+            data = ss_data[:len(ss_data)]+str(root.val)
+            res.append(data)
+            return
+        self.helper(root.left,res,ss_data+str(root.val)+"->")
+        self.helper(root.right,res,ss_data+str(root.val)+"->")
+
