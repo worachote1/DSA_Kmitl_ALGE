2330f357802a79ff601aeedf82d4baa957d7036e
worachote1 <timeworachote2002@gmail.com>
2022-11-19 15:50:51 +0700

    template test

--- CE_Kmitl_Lab/ch9_Sort/ex3.py
+++ CE_Kmitl_Lab/ch9_Sort/ex3.py
@@ -1,4 +1,4 @@
-def solution(data : list):
+def solution(data: list):
     if(check_Metadrome(data)):
         return "Metadrome"
     if(check_Plaindrome(data)):
@@ -11,17 +11,62 @@ def solution(data : list):
         return "Repdrome"
     return "Nondrome"
 
-def check_Metadrome(data : list):
-    pass
-def check_Plaindrome(data : list):
-    pass
-def check_Katadrome(data : list):
-    pass
-def check_Nialpdrome(data : list):
-    pass
-def check_Repdrome(data : list):
-    pass
 
-inp = input("Enter Input : ").split(" ")
+def check_Metadrome(data: list):
+    prev_val = []
+    for i in range(len(data)-1):
+        if(not (data[i] < data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            return False
+        prev_val.append(data[i])
+    return True if data[len(data)-1] not in prev_val else False
+
+def check_Plaindrome(data: list):
+    prev_val = []
+    isDup = False
+    for i in range(len(data)-1):
+        if(not (data[i] <= data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            isDup = True
+        prev_val.append(data[i])
+
+    isLast_Dup = data[len(data)-1] in prev_val
+    return True if isDup or isLast_Dup else False
+
+def check_Katadrome(data: list):
+    prev_val = []
+    for i in range(len(data)-1):
+        if(not (data[i]>data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            return False
+        prev_val.append(data[i])
+    return True if data[len(data)-1] not in prev_val else False
+
+
+def check_Nialpdrome(data : list):
+    prev_val = []
+    isDup = False
+    for i in range(len(data)-1):
+        if(not (data[i]>=data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            isDup = True
+        prev_val.append(data[i])
+    
+    isLast_Dup = data[len(data)-1] in prev_val
+    return True if isDup or isLast_Dup else False
+
+def check_Repdrome(data: list):
+    prev = int(data[0])
+    for item in data:
+        if(prev != int(item)):
+            return False
+    return True
+
+
+inp = input("Enter Input : ")
 data = [int(item) for item in inp]
 print(solution(data))
--- CE_Kmitl_Lab/test_LAB.py
+++ CE_Kmitl_Lab/test_LAB.py
@@ -1,5 +1,57 @@
-import sys
+x = "1357"
+data = [int(item) for item in x]
+def check_Metadrome(data : list):
+    prev_val = []
+    for i in range(len(data)-1):
+        if(not (data[i]<data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            return False
+        prev_val.append(data[i])
+    return True if data[len(data)-1] not in prev_val else False
+x1 = "12344"
+data1 = [int(item) for item in x1]
+def check_Plaindrome(data : list):
+    prev_val = []
+    isDup = False
+    for i in range(len(data)-1):
+        if(not (data[i]<=data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            isDup = True
+        prev_val.append(data[i])
+    
+    isLast_Dup = data[len(data)-1] in prev_val
+    return True if isDup or isLast_Dup else False
 
+x2 = "7531"
+data2 = [int(item) for item in x2]
+def check_Katadrome(data: list):
+    prev_val = []
+    for i in range(len(data)-1):
+        if(not (data[i]>data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            return False
+        prev_val.append(data[i])
+    return True if data[len(data)-1] not in prev_val else False
 
-print(sys.maxsize)
-print(float('inf'))
+x3 = "9874441"
+data3 = [int(item) for item in x3]
+def check_Nialpdrome(data : list):
+    prev_val = []
+    isDup = False
+    for i in range(len(data)-1):
+        if(not (data[i]>=data[i+1])):
+            return False
+        if(data[i] in prev_val):
+            isDup = True
+        prev_val.append(data[i])
+    
+    isLast_Dup = data[len(data)-1] in prev_val
+    return True if isDup or isLast_Dup else False
+
+print(check_Metadrome(data))
+print(check_Plaindrome(data1))
+print(check_Katadrome(data2))
+print(check_Nialpdrome(data3))
