771496f90725e1400ecb663a5614c7748f928bc7
worachote1 <timeworachote2002@gmail.com>
2023-03-16 00:11:15 +0700

    ex 448 : sol 2 use time -> O(n) and space -> o(1)

--- Leet_Code/daily_prn/ex_448.py
+++ Leet_Code/daily_prn/ex_448.py
@@ -1,7 +1,16 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums):
-        return list(set([i for i in range(1,len(nums)+1)]) - set(nums))
-
+        res,n = [],len(nums)
+        for item in nums:
+            temp = abs(item)-1
+            if(nums[temp]>0):
+                nums[temp] *= -1
+        for i in range(n):
+            if(nums[i]>0):
+                res.append(i+1)
+        return res
 test = Solution()
+print(test.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
+print(test.findDisappearedNumbers([1,1]))
 
         
