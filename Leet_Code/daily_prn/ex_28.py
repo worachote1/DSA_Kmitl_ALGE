ebed63ae166a3f2ffe240c297bc205aea99d1f3b
worachote1 <timeworachote2002@gmail.com>
2023-03-15 10:38:53 +0700

    Find Idx first occ : Window Slicing method

--- Leet_Code/daily_prn/ex_28.py
+++ Leet_Code/daily_prn/ex_28.py
@@ -1,10 +1,17 @@
 class Solution(object):
     def strStr(self, haystack, needle):
-        step = len(needle)
-        end = len(haystack)
-        for i in range(0,end):
-            if(i+step > end):
-                break
-            if(needle==haystack[i:i+step]):
-                return i
+        n_haystack,n_needle,Idx_needle = len(haystack),len(needle),0
+        i = 0
+        while(i<n_haystack):
+            if(haystack[i]==needle[Idx_needle]):
+                Idx_needle += 1
+            else:
+                i = i - Idx_needle
+                Idx_needle = 0
+            if(Idx_needle==n_needle):
+                return i-Idx_needle+1
+            i+=1
         return -1
+
+test = Solution()
+print(test.strStr("mississippi","issip"))
