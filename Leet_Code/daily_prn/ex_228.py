4186d649c401685b432fc67a9e06b28eb4c2f645
worachote1 <timeworachote2002@gmail.com>
2023-03-15 14:17:45 +0700

    ex 228

--- Leet_Code/daily_prn/ex_228.py
+++ Leet_Code/daily_prn/ex_228.py
@@ -1,3 +1,26 @@
 class Solution(object):
     def summaryRanges(self, nums):
-        pass
+        Idx,n,res = 0,len(nums),[]
+        while(Idx<n):
+            temp,j = [nums[Idx]],Idx+1
+            while(j<n):
+                x = temp[-1]
+                if(x==nums[j] or x==nums[j]-1):
+                    temp.append(nums[j])
+                    j+=1
+                else:
+                    break
+            Idx = j
+            temp2 = list(set(temp))
+            temp2.sort()
+            # print("temp2 prn : ",temp2)
+            if(len(temp2)>1):
+                res.append(str(temp2[0])+"->"+str(temp2[-1]))
+            else:
+                res.append(str(temp2[0]))
+        return res
+    
+test = Solution()
+print(test.summaryRanges([0,1,2,4,5,7]))
+print(test.summaryRanges([0,2,3,4,6,8,9]))
+print(test.summaryRanges([7,8]))
--- Leet_Code/daily_prn/test_daily_prn.py
+++ Leet_Code/daily_prn/test_daily_prn.py
@@ -1,1 +1,3 @@
-print(ord('๑'))
+temp = [10, 20, 10, 30, 40, 40]
+temp2 = set(temp)
+print(list(temp2))
