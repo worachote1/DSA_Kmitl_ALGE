d62bca7892b3c4754aa05ce60971d8e281a09893
worachote1 <timeworachote2002@gmail.com>
2023-03-25 22:08:39 +0700

    Spiral metri 1

--- Leet_Code/daily_prn/ex_54.py
+++ Leet_Code/daily_prn/ex_54.py
@@ -1,3 +1,30 @@
 class Solution(object):
     def spiralOrder(self, matrix):
-        pass
+        res = []
+        start_row,end_row,start_col,end_col = 0,len(matrix)-1,0,len(matrix[0])-1
+        while(start_row <= end_row and start_col <= end_col):
+            for i in range(start_col,end_col+1):
+                res.append(matrix[start_row][i])
+            start_row += 1
+            if(start_row > end_row):
+                break
+
+            for i in range(start_row,end_row+1):
+                res.append(matrix[i][end_col])
+            end_col -= 1
+            if(start_col > end_col):
+                break               
+            
+            for i in range(end_col,start_col-1,-1):
+                res.append(matrix[end_row][i])
+            end_row -= 1
+            
+            for i in range(end_row,start_row-1,-1):
+                res.append(matrix[i][start_col])
+            start_col += 1
+            
+        return res
+    
+test = Solution()
+print(test.spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
+print(test.spiralOrder([[1,2,3,4],[5,6,7,8],[9,10,11,12]]))
--- Leet_Code/daily_prn/test_daily_prn.py
+++ Leet_Code/daily_prn/test_daily_prn.py
@@ -1,11 +1,2 @@
-class PrefixTree():
-    def __init__(self) :
-        self.children = {}
-        self.isEndOfWord = False
-    
-x= {"a" : PrefixTree()}
- 
-print(list(x))
-# del x["b"]
-del x["a"]
-print((x))
+for i in range(0):
+    print(i)
