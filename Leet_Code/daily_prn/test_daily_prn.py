e80d13297fe70c01aea34b767245fe865a3e3f07
worachote1 <timeworachote2002@gmail.com>
2023-04-01 20:43:57 +0700

    moji gcd

--- Leet_Code/daily_prn/test_daily_prn.py
+++ Leet_Code/daily_prn/test_daily_prn.py
@@ -1,2 +1,30 @@
-for i in range(0):
-    print(i)
+# def gcd(a, b) :
+#      if b == 0 :
+#           return a
+#      else :
+#           return gcd(b, a % b)
+
+# a, b = [abs(int(item)) for item in(input("Enter Input : ").split(" "))]
+# if a < b:
+#      a, b = b, a
+# if a == 0 and b == 0 :
+#      print("Error! must be not all zero.")
+# else :
+#      ans = gcd(a,b)
+#      print(f'The gcd of {a} and {b} is : {ans}'.format(a,b,ans))
+
+def gcd(a,b):
+     if(b == 0):
+          return abs(a)
+     else:
+        return gcd(b, a % b)
+    
+a, b = [(int(item)) for item in(input("Enter Input : ").split(" "))]
+if a < b:
+     a, b = b, a
+if a == 0 and b == 0 :
+     print("Error! must be not all zero.")
+else :
+     ans = gcd(a,b)
+     print(f'The gcd of {a} and {b} is : {ans}'.format(a,b,ans))
+
