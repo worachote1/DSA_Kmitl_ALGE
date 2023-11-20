2eabfbcaf2175fda164c37bf6b081bcaff8c43c4
worachote1 <timeworachote2002@gmail.com>
2023-11-20 22:24:00 +0700

    4 sum with recursion

--- Leet_Code/co-op_prn/ex_18.py
+++ Leet_Code/co-op_prn/ex_18.py
@@ -1,26 +1,61 @@
+# sol 1 : using loop (o(n^3))
+# class Solution(object):
+#     def fourSum(self, nums, target):
+#         res = []
+#         nums.sort()
+#         for i in range(0,len(nums)-3):
+#             if(i != 0 and nums[i] == nums[i-1]):
+#                 continue
+#             for j in range(i+1,len(nums)-2):
+#                 if(j != i+1 and nums[j] == nums[j-1]):
+#                     continue
+#                 pL,pR = j + 1,len(nums)-1
+#                 while(pL < pR):
+#                     sumVal = nums[i]+nums[j]+nums[pL]+nums[pR]
+#                     if(sumVal < target):
+#                         pL += 1
+#                     elif(sumVal > target):
+#                         pR -= 1
+#                     else:
+#                         quadruplet = [nums[i],nums[j],nums[pL],nums[pR]]
+#                         res.append(quadruplet)
+#                         pL += 1
+#                         while(pL < pR and nums[pL] == nums[pL-1]):
+#                             pL += 1
+#         return res
+    
+# sol2 : using recursion(o(n^3)) can be use with any k sum 
 class Solution(object):
     def fourSum(self, nums, target):
         res = []
+        quadruplet = []
         nums.sort()
-        for i in range(0,len(nums)-3):
-            if(i != 0 and nums[i] == nums[i-1]):
-                continue
-            for j in range(i+1,len(nums)-2):
-                if(j != i+1 and nums[j] == nums[j-1]):
-                    continue
-                pL,pR = j + 1,len(nums)-1
+        def kSum(k,startIdx,target):
+            if(k != 2):
+                for i in range(startIdx,len(nums)-k+1):
+                    if(i != startIdx and nums[i] == nums[i-1]):
+                        continue
+                    quadruplet.append(nums[i])
+                    kSum(k-1,i+1,target-nums[i])
+                    quadruplet.pop()
+                    
+            else:
+                pL,pR = startIdx,len(nums)-1
                 while(pL < pR):
-                    sumVal = nums[i]+nums[j]+nums[pL]+nums[pR]
+                    sumVal = nums[pL] + nums[pR]
                     if(sumVal < target):
                         pL += 1
                     elif(sumVal > target):
                         pR -= 1
                     else:
-                        quadruplet = [nums[i],nums[j],nums[pL],nums[pR]]
-                        res.append(quadruplet)
+                        temp_quad = [item for item in quadruplet]
+                        temp_quad += [nums[pL],nums[pR]]
+                        res.append(temp_quad)
                         pL += 1
-                        while(pL < len(nums) and nums[pL] == nums[pL-1]):
+                        while(pL < pR and nums[pL] == nums[pL-1]):
                             pL += 1
+
+        kSum(4,0,target)
         return res
     
 test = Solution()
--- Leet_Code/co-op_prn/test_co-op.py
+++ Leet_Code/co-op_prn/test_co-op.py
@@ -1,4 +1,4 @@
 x = [1,5,3,8,7]
-for i in range(len(x)-1):
-    print(x[i])
-    i += 1
+x += [5,6]
+print(x.copy())
+print([item for item in x])
