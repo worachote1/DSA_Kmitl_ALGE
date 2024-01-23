51f5610e167e7202d271303caa2703dff4c06d0e
worachote1 <timeworachote2002@gmail.com>
2024-01-23 23:32:40 +0700

    prac intersec interval

--- Leet_Code/co-op_prn/ex_57.py
+++ Leet_Code/co-op_prn/ex_57.py
@@ -1,17 +1,17 @@
 class Solution(object):
     def insert(self, intervals, newInterval):
         res = []
-        for i in range(0,len(intervals)):
-            if(newInterval[1] < intervals[i][0]):
+        for i in range(len(intervals)):
+            if(intervals[i][1] < newInterval[0]):
+                res.append(intervals[i])
+            elif(intervals[i][0] > newInterval[1]):
                 res.append(newInterval)
                 return res + intervals[i:]
-            elif(newInterval[0] > intervals[i][1]):
-                res.append(intervals[i])
             else:
-                newInterval[0] = min(newInterval[0],intervals[i][0])
-                newInterval[1] = max(newInterval[1],intervals[i][1])
+                newInterval[0] = min(intervals[i][0],newInterval[0])
+                newInterval[1] = max(intervals[i][1],newInterval[1])
         return res + [newInterval]
-    
+
 test = Solution() 
 print(test.insert([[1,3],[6,9]],[2,5]))
 print(test.insert([[1,2],[3,5],[6,7],[8,10],[12,16]],[4,8]))
