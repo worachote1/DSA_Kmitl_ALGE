237fb1917214a787b5c64206c6b27bd028562c3b
worachote1 <timeworachote2002@gmail.com>
2024-01-23 19:55:50 +0700

    bubble sort merge problem(time exceed)

--- Leet_Code/co-op_prn/ex_56.py
+++ Leet_Code/co-op_prn/ex_56.py
@@ -1,7 +1,23 @@
 class Solution(object):
     def merge(self, intervals):
-          
-    
+        for i in range(len(intervals)):
+            for j in range(len(intervals)-i-1):
+                if(intervals[j][0] > intervals[j+1][0]):
+                    temp = intervals[j]
+                    intervals[j] = intervals[j+1]
+                    intervals[j+1] = temp
+
+        res = [intervals[0]]
+        for i in range(1,len(intervals)):
+            if(intervals[i][0] > res[-1][1]):
+                res.append(intervals[i])
+            else:
+                startIdx = min(res[-1][0],intervals[i][0]) 
+                endIdx = max(res[-1][1],intervals[i][1])
+                res[-1] = [startIdx,endIdx]
+
+        return res
+
 test = Solution()
 print(test.merge([[1,3],[2,6],[8,10],[15,18]]))
 print(test.merge([[1,4],[4,5]]))
