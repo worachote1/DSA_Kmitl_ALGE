54bde677499bf8170ba5930c42d8abd447820f66
worachote1 <worachote659@gmail.com>
2024-12-21 18:27:21 +0700

    backup solution ex_1341: change to CTE metohd

--- Leet_Code/sql_50_study_plan/Subqueries/ex_1341.sql
+++ Leet_Code/sql_50_study_plan/Subqueries/ex_1341.sql
@@ -1,18 +1,14 @@
 # Write your MySQL query statement below
-SELECT name AS results 
-FROM(
+WITH 
+cte_temp_user_table AS (
     SELECT 
         u1.name, 
         COUNT(mr1.rating) OVER (PARTITION BY mr1.user_id) as count_user_rating 
     FROM MovieRating mr1
     LEFT JOIN Users u1 ON mr1.user_id = u1.user_id
     ORDER BY count_user_rating DESC, u1.name ASC LIMIT 1
-) temp_user_table
-
-UNION ALL
-
-SELECT title AS results 
-FROM(
+),
+cte_temp_movie_table AS (
     SELECT 
         m1.title,
         AVG(mr1.rating) OVER (PARTITION BY mr1.movie_id) as avg_movie_rating 
@@ -20,4 +16,8 @@ FROM(
     LEFT JOIN Movies m1 ON mr1.movie_id = m1.movie_id
     WHERE YEAR(mr1.created_at) = 2020 AND MONTH(mr1.created_at) = 2
     ORDER BY avg_movie_rating  DESC, m1.title ASC LIMIT 1
-) temp_movie_table
+)
+
+SELECT name as results FROM cte_temp_user_table
+UNION ALL
+SELECT title as results FROM cte_temp_movie_table
